1c376f51acd7be40e18c43c4dc5508534a57549a
Keenan Romain <keenan.romain@gmail.com>
2020-08-25 22:47:18 -0400

    included basic validation for command line argument, still need to write out usage() function

--- markdown2html.go
+++ markdown2html.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func readFile() []string {
+func readFile(arg string) []string {
 	// see if there is a better/safer way to open a file, particularly when the files are in a subdirectory
-	file, err := os.Open("./input/headerTags.md")
+	file, err := os.Open(arg)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,7 +29,25 @@ func readFile() []string {
 	return markdown
 }
 
+func printUsage() {
+	fmt.Println("Testing...")
+	os.Exit(0)
+}
+
+func validateArgs(args []string) string {
+	if len(args) != 2 {
+		printUsage()
+	}
+	extension := filepath.Ext(args[1])
+	if extension != ".md" {
+		printUsage()
+	}
+	return args[1][strings.Index(args[1], "/")+1 : len(args[1])-len(extension)]
+}
+
 func main() {
-	markdown := readFile()
-	fmt.Println(markdown)
+	args := os.Args
+	fileName := validateArgs(args)
+	markdown := readFile(args[1])
+	fmt.Println(fileName, markdown)
 }
